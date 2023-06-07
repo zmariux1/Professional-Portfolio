ab8a852879584b072753d125467f4a9389b4f46a
zmariux1 <69869887+zmariux1@users.noreply.github.com>
2023-06-07 17:07:33 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
+// ===================== toggle icon navbar ===================== 
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+}
+
+// ===================== scroll section active link ===================== 
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    };
+  });
+  // ===================== Sticky nav bar ===================== 
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100)
+
+  // ===================== REMOVE taggle Icon & NavBar when clicked NavBar link ===================== 
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active")
+};
+
+
+// ===================== Scroll Reveal ===================== 
+
+
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, contract form", { origin: "bottom" });
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+
+// ===================== typed js ===================== 
+const typed = new Typed(".multuple-text", {
+  strings: ["Web Developer", "Frontend Dev", "Backend Dev"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
